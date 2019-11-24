f1570444e6f458f6c3dbca855c05ddb8f3470de6
105820006 <oscarada87@gmail.com>
2019-11-24 14:48:12 +0800

    add NavBar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">ITS</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="mr-auto">
+          <b-nav-item-dropdown text="Dashboard" right>
+            <b-dropdown-item href="#">123</b-dropdown-item>
+            <b-dropdown-item href="#">456</b-dropdown-item>
+            <b-dropdown-item href="#">789</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <!-- <b-nav-form>
+            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+          </b-nav-form> -->
+          <b-button variant='transparent'>
+            <font-awesome-icon icon="info-circle" size="lg" />
+          </b-button>
+
+          <b-nav-item-dropdown right>
+            <template v-slot:button-content>
+              <font-awesome-icon icon="user" size="lg" />
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUser, faSearch, faInfoCircle } from '@fortawesome/free-solid-svg-icons'
+// import { faQuestionCircle } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+library.add(faUser, faInfoCircle, faSearch)
+Vue.use(BootstrapVue)
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false
 
