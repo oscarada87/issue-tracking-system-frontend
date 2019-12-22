20a6a0b288fe07439d85b8006a1ff62b8be9d0ad
zxjte9411 <a5852241@gmail.com>
2019-12-22 15:52:09 +0800

    加一些 dropdown 進去

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
     <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#">ITS</b-navbar-brand>
-
+      <router-link to="/">
+        <b-navbar-brand>ITS</b-navbar-brand>
+      </router-link>
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav class="mr-auto">
-          <b-nav-item-dropdown text="Dashboard" right>
+          <b-nav-item-dropdown text="Dashboard" left>
             <b-dropdown-item href="#">123</b-dropdown-item>
             <b-dropdown-item href="#">456</b-dropdown-item>
             <b-dropdown-item href="#">789</b-dropdown-item>
+          </b-nav-item-dropdown>
+          <b-nav-item-dropdown text="Project" left>
+            <b-dropdown-item @click="$bvModal.show('modal-scoped')">create</b-dropdown-item>
+            <b-dropdown-item href="#">my project</b-dropdown-item>
           </b-nav-item-dropdown>
         </b-navbar-nav>
 
@@ -19,8 +24,8 @@
           <!-- <b-nav-form>
             <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
             <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-          </b-nav-form> -->
-          <b-button variant='transparent'>
+          </b-nav-form>-->
+          <b-button variant="transparent">
             <font-awesome-icon icon="info-circle" size="lg" />
           </b-button>
 
@@ -39,8 +44,18 @@
 
 <script>
 export default {
-  name: 'NavBar'
-}
+  name: "NavBar",
+  Data(){
+    return {
+
+    }
+  },
+  methods:{
+    createProject(){
+
+    },
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
