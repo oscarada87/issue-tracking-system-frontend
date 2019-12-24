88f7628ee5eada35c49381764aee20d7b1dc11f8
zxjte9411 <a5852241@gmail.com>
2019-12-25 02:11:54 +0800

    登入後顯示使用者的 Name

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -13,21 +13,14 @@
             <b-dropdown-item href="#">456</b-dropdown-item>
             <b-dropdown-item href="#">789</b-dropdown-item>
           </b-nav-item-dropdown>
-          <!-- <b-nav-item-dropdown text="Project" left>
-            <b-dropdown-item @click="$bvModal.show('modal-scoped')">create</b-dropdown-item>
-            <b-dropdown-item @click="goMyProject">my project</b-dropdown-item>
-          </b-nav-item-dropdown>-->
-          <b-nav-item href="#/project">Project</b-nav-item>
+          <b-nav-item v-if="isShow" href="#/project">Project</b-nav-item>
           <b-nav-item href="#/issue">Issue</b-nav-item>
           <b-nav-item v-if="isShow" href="#/accountmanagement">AccountManagement</b-nav-item>
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
-          <!-- <b-nav-form>
-            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-          </b-nav-form>-->
+          <b-nav-item >{{userName}}</b-nav-item>
           <b-button variant="transparent">
             <font-awesome-icon icon="info-circle" size="lg" />
           </b-button>
@@ -51,6 +44,7 @@ export default {
   name: "NavBar",
   data() {
     return {
+      userName: "",
       isShow: false
     };
   },
@@ -78,6 +72,7 @@ export default {
         .catch(err => {
           return err.response;
         });
+      this.userName = res.data.name;
       if (res.data.charactorId == 1) this.isShow = true;
       else this.isShow = false;
     }
