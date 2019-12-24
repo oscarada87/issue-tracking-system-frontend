8810dbf074da1f0db4e9037deed339151bdaad03
zxjte9411 <a5852241@gmail.com>
2019-12-25 04:53:56 +0800

    改字體

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Noto Sans TC', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
